985bbce01f9e121c4ec50ba7bdca55b38358eed7
Van Den Hout Benjamin, SUB-SDC-NL-DOS-OCD <Benjamin.VanDenHout@swisscom.com>
2024-12-13 18:33:57 +0100

    Initial Commit

---
+++ NavigationTest/NavigationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTestApp.swift
+//  NavigationTest
+//
+//  Created by Benjamin van den Hout on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
