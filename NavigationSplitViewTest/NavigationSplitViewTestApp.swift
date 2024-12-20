98ecfe740bfee517127f2203f79ec38b808a83cd
Van Den Hout Benjamin, SUB-SDC-NL-DOS-OCD <Benjamin.VanDenHout@swisscom.com>
2024-12-20 14:54:23 +0100

    Add menu

--- NavigationSplitViewTest/NavigationSplitViewTestApp.swift
+++ NavigationSplitViewTest/NavigationSplitViewTestApp.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 @main
 struct NavigationSplitViewTestApp: App {
+    enum ColumnView {
+        case none
+        case twoColumn
+        case threeColumn
+    }
+
     @StateObject private var model = Model()
+    @State private var selectedColumn: ColumnView = .none
 
     var body: some Scene {
         WindowGroup {
-//            TwoColumnView()
-            ThreeColumnView()
-                .environmentObject(model)
+            switch selectedColumn {
+            case .none:
+                VStack {
+                    Text("Select desired column view")
+                        .font(.title)
+                        .padding()
+
+                    Group {
+                        Button("Two columns") {
+                            selectedColumn = .twoColumn
+                        }
+                        Button("Three columns") {
+                            selectedColumn = .threeColumn
+                        }
+                    }
+                    .buttonStyle(.bordered)
+                    .padding()
+                }
+            case .twoColumn:
+                TwoColumnView()
+                    .environmentObject(model)
+            case .threeColumn:
+                ThreeColumnView()
+                    .environmentObject(model)
+
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 The purpose of this project is to get more familiar with `NavigationSplitView` in SwiftUI.
 
-Inside `NavigationSplitViewTestApp` you can select if you want the 2 or 3 column version.
-
 Features:
 
+- Both 2 and 3 column layouts are available.
 - Project should work both on iPhone and iPad.
 - Optional behaviour (enabled by default, see `autoSelectFirstItemOnPad`) to auto select the first `Item` on iPad because to me this looks a bit better.
 - Swipe to delete and Edit mode are supported.
